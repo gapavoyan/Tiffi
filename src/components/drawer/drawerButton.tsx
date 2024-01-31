fc2ca476795f0e427060d28c0ff6426f42717267
Gap Avoyan <gap.avoyan.ait@gmai.com>
2024-01-31 13:54:42 +0400

    create drawer menu  icon change on click

--- src/components/drawer/drawerButton.tsx
+++ src/components/drawer/drawerButton.tsx
@@ -1,9 +1,22 @@
-import React from "react";
 import Image from "next/image";
-function DrawerButton() {
+import React, { useState } from "react";
+interface Props {
+  onClick: () => void;
+  isDrawerMenuOpen: Boolean;
+}
+
+function DrawerButton({ onClick, isDrawerMenuOpen }: Props) {
   return (
-    <div className="hidden max-lg:block border-customBlack border-[1px] rounded-[100px] px-[12px] py-[12px]">
-      <Image src="/icon/icon.svg" alt="" width={15} height={15} />
+    <div
+      className="hidden max-lg:block border-customBlack border-[1px] rounded-[100px] px-[12px] py-[12px] cursor-pointer"
+      onClick={onClick}
+    >
+      <Image
+        src={`/icon/${isDrawerMenuOpen ? "iconMin" : "icon"}.svg`}
+        alt=""
+        width={15}
+        height={15}
+      />
     </div>
   );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../navbar/navbar";
 import Search from "../search/search";
 import DrawerButton from "../drawer/drawerButton";
 
 function Header() {
+  const [isDrawerMenuOpen, setIsDrawerMenuOpen] = useState(false);
+  const toggleIsDrawerMenuOpen = () => {
+    setIsDrawerMenuOpen((isOpen) => !isOpen);
+  };
   return (
     <>
-      <header className="border-b-[1px] border-customBlack w-full">
+      <header className=" w-full">
         <div className="max-sm:gap-4 mx-[252px] my-[24px] max-xl:mx-[175px] max-lg:mx-[142px] max-md:mx-[73px] max-sm:flex-col max-sm:mx-[16px]  flex justify-between">
           <div className="flex justify-between items-center w-full">
             <Link href="/">
@@ -17,12 +21,16 @@ function Header() {
             <div className=" max-lg:hidden">
               <Navbar />
             </div>
-            <DrawerButton />
+            <DrawerButton
+              onClick={toggleIsDrawerMenuOpen}
+              isDrawerMenuOpen={isDrawerMenuOpen}
+            />
           </div>
           <div className="flex ml-[180px] max-xl:ml-[120px] max-mij:ml-[30px] max-lg:ml-[12px] max-sm:ml-0 items-center ">
             <Search />
           </div>
         </div>
+        <div className="w-full border-b-[1px] border-customBlack"></div>
       </header>
     </>
   );
