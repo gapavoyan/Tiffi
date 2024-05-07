7acc48c31935f83124ef291c6431ae3b1883fad9
Gap Avoyan <gap.avoyan.ait@gmai.com>
2024-05-07 10:44:03 +0400

    add siper scroll to activ button

--- src/components/slider/slider-subcategory-button/slider-subCategory.tsx
+++ src/components/slider/slider-subcategory-button/slider-subCategory.tsx
@@ -1,10 +1,14 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useRef, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
 import "swiper/css";
 import Button from "@/components/buttons/subcategoriesFilter-button";
 import { useRouter } from "next/router";
 import { T_Brand } from "@/dataBase/dataBrands";
 import { Gender } from "@/hooks/useHeaderInfo";
+import { Navigation } from "swiper/modules";
+
+SwiperCore.use([Navigation]);
 
 interface Props {
   activeId?: number | null;
@@ -16,12 +20,30 @@ interface Props {
 
 export default function SliderSubCategory({ activeId, parentId, onChangeSubcategory, data, gender }: Props) {
   const router = useRouter();
+  const swiperRef = useRef<SwiperCore | null>(null);
+
   const filteredBrands = useMemo(() => {
     if (!gender) return data;
     return data?.filter(brand => brand.gender === gender);
   }, [data, gender]);
 
   const showAllButton = useMemo(() => !!parentId, [parentId]);
+
+  const activeButtonIndex = useMemo<number>(() => {
+    if (!activeId) return 0;
+    const index = filteredBrands?.findIndex(item => item.id === activeId);
+    return index !== undefined && index >= 0 ? index : 0;
+  }, [activeId, filteredBrands]);
+
+  useEffect(() => {
+    if (swiperRef.current) {
+      swiperRef.current.slideTo(activeButtonIndex);
+    }
+  }, [activeButtonIndex]);
+
+  const onSwiperHandler = (swiper: SwiperCore) => {
+    swiperRef.current = swiper;
+  };
   const onSubCategoryItemClick = (id: number) => {
     if (onChangeSubcategory) onChangeSubcategory(id);
     if (parentId && gender) router.push(`/category/${parentId}/subcategories/${id}?gender=${gender}`);
@@ -29,7 +51,13 @@ export default function SliderSubCategory({ activeId, parentId, onChangeSubcateg
   };
 
   return (
-    <Swiper navigation={true} slidesPerView={"auto"} spaceBetween={10} className="mySwiper slider-category">
+    <Swiper
+      navigation={true}
+      slidesPerView={"auto"}
+      spaceBetween={10}
+      className="mySwiper slider-category"
+      onSwiper={onSwiperHandler}
+    >
       {showAllButton && (
         <SwiperSlide className="swiper-slide">
           <Button onClick={() => onSubCategoryItemClick(parentId!)} isActive={activeId === parentId}>
